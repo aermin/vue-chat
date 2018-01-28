138968cad05e07d5d3528ab7d6a6668e1c3cf965
Hxvin <13055503310@163.com>
2018-01-28 22:57:12 +0800

    首页列表前端接入api数据

--- server/controllers/message.js
+++ server/controllers/message.js
@@ -2,40 +2,26 @@ const jwt = require("jsonwebtoken");
 const secret = require("../config").secret;
 const msgModel = require("../models/message");
 
-module.exports = {
-    getGroupList: async(ctx, next) => {
-        const auth = ctx.get("Authorization");
-        const token = auth.split(" ")[1];
-        const payload = jwt.verify(token, secret);
-        const user_id = payload.id;
-        const groupMessage = await msgModel.getGroupList(user_id);
-        return groupMessage
-    },
-    getPrivateList : async(ctx,next) =>{
-        const auth = ctx.get("Authorization");
-        const token = auth.split(" ")[1];
-        const payload = jwt.verify(token, secret);
-        const user_id = payload.id;
-        const groupMessage = await msgModel.getPrivateList(user_id);
-        return groupMessage
-    },
-
-    getAllList: async(ctx,next)=>{
-        try {
-            const privateList = await getGroupList();
-            const groupList = await getGroupList();
-            console.log("privateList    " , privateList)
-            console.log("groupList    " , groupList)
-            ctx.body = {
-                success: true,
-                data: {
-                    privateList,
-                    groupList
-                }
-              };
-        } catch (error) {
-            console.log(error)
-        }        
-    }
-
-}
+module.exports = async (ctx, next) => {
+  try {
+    const auth = ctx.get("Authorization");
+    const token = auth.split(" ")[1];
+    const payload = jwt.verify(token, secret);
+    const user_id = payload.id;
+    const res1 = await msgModel.getPrivateList(user_id);
+    const privateList = JSON.parse(JSON.stringify(res1));
+    const res2 = await msgModel.getGroupList(user_id);
+    const groupList = JSON.parse(JSON.stringify(res2));
+    console.log("privateList    ", privateList);
+    console.log("groupList    ", groupList);
+    ctx.body = {
+      success: true,
+      data: {
+        privateList: privateList,
+        groupList: groupList
+      }
+    };
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import axios from "axios";
+import { toNomalTime } from "../utils/transformTime";
 
 Vue.use(Vuex);
 
@@ -13,61 +14,62 @@ const store = new Vuex.Store({
         user: "robot"
       }
     ],
-    chatList:[
-        {
-          name:"技术交流群",
-            img:"../../static/peoples1.jpg",
-            type:"group",
-            id:"",
-            message:"大家好，我是22",
-            time:"2017-12-12",
-            unread:"0"
-        },
-        {
-            name:"闲聊群",
-              img:"../../static/peoples2.jpg",
-              type:"group",
-              id:"",
-              message:"闲聊群，比较适合我",
-              time:"2017-11-11",
-              unread:"0"
-        },
-        {
-            name:"路飞",
-              img:"../../static/people1.jpg",
-              type:"private",
-              id:"",
-              message:"这条是22回复11的",
-              time:"2017-11-11",
-              unread:"0"
-        },
-        {
-            name:"索隆",
-              img:"../../static/people2.jpg",
-              type:"private",
-              id:"",
-              message:"这条是11发给33的",
-              time:"2017-11-11",
-              unread:"0"
-        },
+    msgList: [
+      // {
+      //   name:"技术交流群",
+      //     img:"../../static/peoples1.jpg",
+      //     type:"group",
+      //     id:"",
+      //     message:"大家好，我是22",
+      //     time:"2017-12-12",
+      //     unread:"0"
+      // },
+      // {
+      //     name:"闲聊群",
+      //       img:"../../static/peoples2.jpg",
+      //       type:"group",
+      //       id:"",
+      //       message:"闲聊群，比较适合我",
+      //       time:"2017-11-11",
+      //       unread:"0"
+      // },
+      // {
+      //     name:"路飞",
+      //       img:"../../static/people1.jpg",
+      //       type:"private",
+      //       id:"",
+      //       message:"这条是22回复11的",
+      //       time:"2017-11-11",
+      //       unread:"0"
+      // },
+      // {
+      //     name:"索隆",
+      //       img:"../../static/people2.jpg",
+      //       type:"private",
+      //       id:"",
+      //       message:"这条是11发给33的",
+      //       time:"2017-11-11",
+      //       unread:"0"
+      // }
     ]
   },
   getters: {
     robotMsgGetter: state => state.robotmsg,
-    chatListGetter:state => state.chatList
+    msgListGetter: state => state.msgList
   },
   mutations: {
     robotMsgMutation(state, data) {
-      state.robotmsg.push(data)
+      state.robotmsg.push(data);
     },
-    chatListMutation(state, data) {
-    state.chatList.push(data)
+    msgListMutation(state, data) {
+      state.msgList = data;
     }
   },
   actions: {
     robatMsgAction({ commit }, data) {
       console.log(data + "  robatMsgAction");
-      axios.get("/api/v1/robot", {
+      axios
+        .get("/api/v1/robot", {
           params: data
         })
         .then(res => {
@@ -79,7 +81,10 @@ const store = new Vuex.Store({
               });
             } else if (res.data.data.code === 200000) {
               let data = res.data.data.text + res.data.data.url;
-              commit("robotMsgMutation", { message: data, user: "robot" });
+              commit("robotMsgMutation", {
+                message: data,
+                user: "robot"
+              });
             } else if (res.data.data.code === 302000) {
               commit("robotMsgMutation", {
                 message: "暂不支持此类对话",
@@ -96,8 +101,29 @@ const store = new Vuex.Store({
         .catch(err => {
           console.log(err);
         });
+    },
+    async msgListAction({ commit }) {
+      const res = await axios.get("/api/v1/message");
+      console.log("res", res);
+      if (res.data.success) {
+        const groupList = res.data.data.groupList;
+        const privateList = res.data.data.privateList;
+        groupList.forEach(element => {
+          element.type = "group";
+          element.time = toNomalTime(element.time);
+        });
+        privateList.forEach(element => {
+          element.type = "private";
+          element.time = toNomalTime(element.time);
+        });
+        const allMsgList = groupList.concat(privateList);
+        allMsgList.sort((a, b) => {
+          return b.time - a.time;
+        });
+        console.log("allMsgList", allMsgList);
+        commit("msgListMutation", allMsgList);
+      }
     }
   }
 });
-
 export default store;
