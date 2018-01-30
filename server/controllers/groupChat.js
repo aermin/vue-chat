83e7b5fcf5e80c5b852bd1e4b48d19837510b0fd
Hxvin <13055503310@163.com>
2018-01-30 10:21:25 +0800

    feat:群聊信息前端展示

--- server/controllers/groupChat.js
+++ server/controllers/groupChat.js
@@ -1,40 +1,23 @@
 const groupChatModel = require("../models/groupChat");
 
-module.exports ={
-    groupMsg:  async (ctx, next) => {
-        try {
-          const groupId = ctx.query.group_id;
-          const RowDataPacket = await groupChatModel.getGroupMsg(groupId);
-          const groupMsg = JSON.parse(JSON.stringify(RowDataPacket));
-          console.log("groupMsg    ", groupMsg);
-          ctx.body = {
-            success: true,
-            data: {
-              groupMsg: groupMsg
-            }
-          };
-        } catch (error) {
-          console.log(error);
-        }
-    },
-    groupDetail:  async (ctx, next) => {
-        try {
-          const groupId = ctx.query.group_id ,
-                RowDataPacket1 = await groupChatModel.getGroupMember(groupId),
-                RowDataPacket2 = await groupChatModel.getGroupInfo(groupId),
-                groupMember = JSON.parse(JSON.stringify(RowDataPacket1)),
-                groupInfo  = JSON.parse(JSON.stringify(RowDataPacket2));
-          console.log("groupMember    ", groupMember);
-          console.log("groupInfo    ", groupInfo);
-          ctx.body = {
-            success: true,
-            data: {
-                groupMember: groupMember,
-                groupInfo:groupInfo
-            }
-          };
-        } catch (error) {
-          console.log(error);
-        }
-    },
-} 
+module.exports = async (ctx, next) => {
+  try {
+    const groupId = ctx.query.groupId,
+    RowDataPacket1 = await groupChatModel.getGroupMsg(groupId),
+    RowDataPacket2 = await groupChatModel.getGroupInfo(groupId),
+    RowDataPacket3 = await groupChatModel.getGroupMember(groupId),
+    groupMsg = JSON.parse(JSON.stringify(RowDataPacket1)),
+    groupInfo = JSON.parse(JSON.stringify(RowDataPacket2));
+    groupMember = JSON.parse(JSON.stringify(RowDataPacket3)),
+    ctx.body = {
+      success: true,
+      data: {
+        groupMsg: groupMsg,
+        groupInfo:groupInfo,
+        groupMember:groupMember
+      }
+    };
+  } catch (error) {
+    console.log(error);
+  }
+}
