a305357ca0c49fa59502f26205a94f2dc83ab8ef
Hxvin <13055503310@163.com>
2018-02-22 18:42:29 +0800

    test

--- config/index.js
+++ config/index.js
@@ -42,7 +42,7 @@ module.exports = {
 
 		// Paths
 		assetsRoot: path.resolve(__dirname, '../dist'),
-		assetsSubDirectory: '../dist/static',
+		assetsSubDirectory: 'static',
 		assetsPublicPath: '/airchat/dist',
 
 		/**
