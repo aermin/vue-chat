28130dbde4066610ab47a4d31ee26e7e0a03eed1
Hxvin <13055503310@163.com>
2018-02-23 20:47:15 +0800

    feat：打包上线

--- server/index.js
+++ server/index.js
@@ -2,61 +2,69 @@ const Koa = require("koa");
 const bodyParser = require("koa-bodyparser");
 const cors = require("koa2-cors");
 const dbConfig = require("./config").db;
-const router = require("./routes/index");
-const { query } = require("./utils/db");
+const router = require("./routes/index"); //路由依赖的中间间
+const static = require('koa-static') //静态资源服务插件
+const path = require('path') //路径管理
+const {
+	query
+} = require("./utils/db");
 const socketModel = require("./models/soketHander");
 const app = new Koa();
 
-// const server = require('http').Server(app.callback());
-// const io = require('socket.io')(server);
 const server = require("http").createServer(app.callback());
 const io = require("socket.io")(server);
-// io.on('connection', function(){ /* … */ });
+
 server.listen(3000);
 
 app.use(cors());
 
 app.use(bodyParser());
 
+// 配置静态资源
+const staticPath = '../dist'
+app.use(static(
+	path.join(__dirname, staticPath)
+))
+
 app.use(router.routes()).use(router.allowedMethods());
 
 global.query = query;
 
 io.on("connection", socket => {
-  const socketId = socket.id;
-  //登录
-  socket.on("login", async userId => {
-    await socketModel.saveUserSocketId(userId, socketId);
-  });
-  // 更新soketId 
-  socket.on("update", async userId => {
-    await socketModel.saveUserSocketId(userId, socketId);
-  });
- //私聊
-  socket.on("sendPrivateMsg", async data => {
-    const arr = await socketModel.getUserSocketId(data.to_user);
-    const RowDataPacket = arr[0];
-    const socketid = JSON.parse(JSON.stringify(RowDataPacket)).socketid;
-    io.to(socketid).emit("getPrivateMsg", data);
-  });
- // 群聊
-  socket.on("sendGroupMsg", async data => {
-    io.sockets.emit("getGroupMsg", data);
-  });
+	const socketId = socket.id;
+	//登录
+	socket.on("login", async userId => {
+		await socketModel.saveUserSocketId(userId, socketId);
+	});
+	// 更新soketId
+	socket.on("update", async userId => {
+		await socketModel.saveUserSocketId(userId, socketId);
+	});
+	//私聊
+	socket.on("sendPrivateMsg", async data => {
+		const arr = await socketModel.getUserSocketId(data.to_user);
+		const RowDataPacket = arr[0];
+		const socketid = JSON.parse(JSON.stringify(RowDataPacket)).socketid;
+		io.to(socketid).emit("getPrivateMsg", data);
+	});
+	// 群聊
+	socket.on("sendGroupMsg", async data => {
+		io.sockets.emit("getGroupMsg", data);
+	});
 
- //加好友请求
-  socket.on("sendRequest", async data => {
-    console.log("sendRequest", data);
-    const arr = await socketModel.getUserSocketId(data.to_user);
-    const RowDataPacket = arr[0];
-    const socketid = JSON.parse(JSON.stringify(RowDataPacket)).socketid;
-    console.log('给谁的socketid',socketid)
-    io.to(socketid).emit("getresponse", data);
-  });
+	//加好友请求
+	socket.on("sendRequest", async data => {
+		console.log("sendRequest", data);
+		const arr = await socketModel.getUserSocketId(data.to_user);
+		const RowDataPacket = arr[0];
+		const socketid = JSON.parse(JSON.stringify(RowDataPacket)).socketid;
+		console.log('给谁的socketid', socketid)
+		io.to(socketid).emit("getresponse", data);
+	});
 
-  socket.on("disconnect", data => {
-    console.log("disconnect", data);
-  });
+	socket.on("disconnect", data => {
+		console.log("disconnect", data);
+	});
 });
 
 // app.listen(3000);
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,103 +1,103 @@
 <template>
-    <!--  登录 -->
-    <div class="login">
-        <Message-box :visible="this.messageBox.visible" :messageBoxEvent="this.messageBox.messageBoxEvent" @confirm="confirm" :hasCancel= false>
-            <p slot="content">{{this.messageBox.message}}</p>
-        </Message-box>
-        <div class="wrapper fadeInDown">
-            <div id="formContent">
-                <h2 class="active"> 登录 </h2>
-                <h2 class="inactive underlineHover" @click="$router.push('/register')">注册 </h2>
-    
-                <div class="fadeIn first">
-                    <img src="../assets/icon.svg" id="icon" alt="User Icon" />
-                </div>
-    
-                <form>
-                    <input type="text" class="fadeIn second" placeholder="用户名" v-model="name">
-                    <input type="text" class="fadeIn third" placeholder="密码" v-model="password">
-                    <input type="submit" @click="login" class="fadeIn fourth" value="登录">
-                </form>
-            </div>
-        </div>
-    </div>
+<!--  登录 -->
+<div class="login">
+	<Message-box :visible="this.messageBox.visible" :messageBoxEvent="this.messageBox.messageBoxEvent" @confirm="confirm" :hasCancel=f alse>
+		<p slot="content">{{this.messageBox.message}}</p>
+	</Message-box>
+	<div class="wrapper fadeInDown">
+		<div id="formContent">
+			<h2 class="active"> 登录 </h2>
+			<h2 class="inactive underlineHover" @click="$router.push('/register')">注册 </h2>
+
+			<div class="fadeIn first">
+				<img src="../assets/icon.svg" id="icon" alt="User Icon" />
+			</div>
+
+			<form>
+				<input type="text" class="fadeIn second" placeholder="用户名" v-model="name">
+				<input type="text" class="fadeIn third" placeholder="密码" v-model="password">
+				<input type="submit" @click="login" class="fadeIn fourth" value="登录">
+			</form>
+		</div>
+	</div>
+</div>
 </template>
 
 <script>
-    import axios from "axios";
-    export default {
-        name: "login",
-        props: {},
-        components: {},
-        data() {
-            return {
-                name: "",
-                password: "",
-                // visible: false,
-                // message: "",
-                messageBox: {
-                    visible: false,
-                    message: "", //弹窗内容
-                    hasCancel: true, //弹窗是否有取消键
-                    messageBoxEvent: "" // 弹窗事件名称
-                }
-            };
-        },
-    
-        computed: {},
-    
-        watch: {},
-    
-        methods: {
-            login() {
-                if (this.name !== "" && this.password !== "") {
-                    axios.post("/api/v1/login", {
-                            name: this.name,
-                            password: this.password
-                        })
-                        .then(res => {
-                            if (res.data.success) {
-                                //保存soket.io
-                                socket.emit('login', res.data.userInfo.user_id)
-                                localStorage.setItem("userToken", res.data.token);
-                                localStorage.setItem("userInfo", JSON.stringify(res.data.userInfo));
-                                //弹窗
-                                this.messageBox.messageBoxEvent = 'login'
-                                this.messageBox.visible = true;
-                                this.messageBox.message = "您已登录成功"
-                            } else {
-                                this.$message({
-                                    message: res.data.message,
-                                    type: "error"
-                                });
-                            }
-                        })
-                        .catch(err => {
-                            const errorMsg = err.response.data.error;
-                            this.$message({
-                                message: errorMsg,
-                                type: "error"
-                            });
-                        });
-                } else {
-                    const message = this.name === "" ? "请输入用户名" : "请输入密码";
-                    this.$message({
-                        message: message,
-                        type: "warn"
-                    });
-                }
-            },
-            confirm(value) {
-                console.log('confirm',value)
-                if (value === 'login') {
-                    this.messageBox.visible = false;
-                    this.$router.push("/message");
-                }
-            }
-        }
-    };
+import axios from "axios";
+export default {
+	name: "login",
+	props: {},
+	components: {},
+	data() {
+		return {
+			name: "",
+			password: "",
+			// visible: false,
+			// message: "",
+			messageBox: {
+				visible: false,
+				message: "", //弹窗内容
+				hasCancel: true, //弹窗是否有取消键
+				messageBoxEvent: "" // 弹窗事件名称
+			}
+		};
+	},
+
+	computed: {},
+
+	watch: {},
+
+	methods: {
+		login() {
+			if (this.name !== "" && this.password !== "") {
+				axios.post("/api/v1/login", {
+						name: this.name,
+						password: this.password
+					})
+					.then(res => {
+						if (res.data.success) {
+							//保存soket.io
+							socket.emit('login', res.data.userInfo.user_id)
+							localStorage.setItem("userToken", res.data.token);
+							localStorage.setItem("userInfo", JSON.stringify(res.data.userInfo));
+							//弹窗
+							this.messageBox.messageBoxEvent = 'login'
+							this.messageBox.visible = true;
+							this.messageBox.message = "您已登录成功"
+						} else {
+							this.$message({
+								message: res.data.message,
+								type: "error"
+							});
+						}
+					})
+					.catch(err => {
+						console.log(err)
+						// this.$message({
+						// 	message: err,
+						// 	type: "error"
+						// });
+					});
+			} else {
+				const message = this.name === "" ? "请输入用户名" : "请输入密码";
+				this.$message({
+					message: message,
+					type: "warn"
+				});
+			}
+		},
+		confirm(value) {
+			console.log('confirm', value)
+			if (value === 'login') {
+				this.messageBox.visible = false;
+				this.$router.push("/message");
+			}
+		}
+	}
+};
 </script>
 
 <style lang="scss" scoped>
-    @import "../assets/loginregister.scss";
+@import "../assets/loginregister.scss";
 </style>
